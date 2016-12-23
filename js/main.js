944e0810eb595a2edcbe429fe21f5ba87c41367c
Abdul Raoof <mailtoraoof@gmail.com>
2016-12-23 15:27:12 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#ddd',
+    lineColor: '#ddd'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+});
